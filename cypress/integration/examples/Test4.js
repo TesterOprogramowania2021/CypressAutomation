6fde2ea07216b5352b1518702cc794178202eb72
Szymon Wasiak <wasiak.szymon.1991@gmail.com>
2021-03-21 20:11:04 +0100

    nowe metody i testy

--- cypress/integration/examples/Test4.js
+++ cypress/integration/examples/Test4.js
@@ -1,29 +1,47 @@
 /// <reference types="Cypress" />
  
-describe('My Second Test Suite', function() 
+describe('My fourth Test Suite', function() 
 {
  
-it('My fourth case',function() {
+it('My FirstTest case',function() {
  
-//alerts
-cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
-cy.get("#alertbtn").click();
-cy.get("[value='Confirm']").click();
- //widnow alert
- cy.on("window:alert",(str)=>
- {
-     //Mocha
-     expect(str).to.equal("Hello , share this practice page and share your knowledge");
- })
- cy.on("window:confirm",(str)=>
- {
-     //Mocha
-     expect(str).to.equal("Hello , Are you sure you want to confirm?");
- })
- //delate atrubiut page to opene page in another window, now open in the same window 
- cy.get("#opentab").invoke("removeAttr","target").click();
- //back to normal/ first page
- cy.go("back");
+//Check boxes
+cy.visit("http://qaclickacademy.com/practice.php")
+cy.get('#alertbtn').click()
+cy.get('[value="Confirm"]').click()
+//window:alert
+cy.on('window:alert',(str)=>
+{
+    //Mocha
+    expect(str).to.equal('Hello , share this practice page and share your knowledge')
+})
+ 
+cy.on('window:confirm',(str)=>
+{
+    //Mocha
+    expect(str).to.equal('Hello , Are you sure you want to confirm?')
+})
+ 
+cy.get('#opentab').invoke('removeAttr','target').click()
+ 
+cy.url().should('include','qaclickacademy')
+ 
+cy.go('back')
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
  
  
